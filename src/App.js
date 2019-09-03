34440f8a781108580c742c734ac9e3f99f20c7be
Axel Roffi <axel.roffi@gmail.com>
2019-09-03 17:57:10 +0200

    components: navbar, UserItem structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar.js";
+import UserItem from "./components/users/UserItem.js";
 import "./App.css";
 
 class App extends React.Component {
-  foo = () => "bars";
   render() {
-    const name = "Axel Roffi";
-    const loading = false;
-    const showName = true;
     return (
-      <div className='App'>
-        {loading ? <h4>LOADING...</h4> : <h1>Hello {showName && name}</h1>}
+      <div>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class UserItem extends Component {
+  state = {
+    id: 1,
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo"
+  };
+  render() {
+    const { avatar_url, login, html_url } = this.state;
+
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: "60px" }}
+        ></img>
+        <h3>{login}</h3>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          MORE
+        </a>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
