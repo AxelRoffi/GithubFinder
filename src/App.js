7df09b0b0f1b712cf14bda7d3f1c08b08394c18b
Axel Roffi <axel.roffi@gmail.com>
2019-09-04 16:32:44 +0200

    refactor axios and clean code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar.js";
 import Users from "./components/users/Users.js";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-  componentDidMount(){
-    
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+    this.setState({
+      users: res.data,
+      loading: false
+    });
   }
   render() {
     return (
       <div>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
